b64072cb3baee42ef35329dc2c330807381e6a70
Andy Hoang <ngocdunghoangdl@gmail.com>
2016-05-08 14:20:46 +0700

    merging 2 repo again

---
+++ asciidoc-maven/src/main/java/com/aavn/wow/andhoang/App.java
@@ -0,0 +1,13 @@
+package com.aavn.wow.andhoang;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
